a214bc9b3ac37cd2e1ceb353803b5a26a0a744bb
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-18 10:03:44 +0300

    export header and footer components from core module

--- trip-forum/src/app/core/core.module.ts
+++ trip-forum/src/app/core/core.module.ts
@@ -3,15 +3,9 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-
-
 @NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [FooterComponent, HeaderComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
