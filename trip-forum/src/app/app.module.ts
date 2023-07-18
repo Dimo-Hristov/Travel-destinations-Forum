7157f00d6b5326c3fcca7b33f863d15ae7474946
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-18 10:20:04 +0300

    import HttpClient in api service

--- trip-forum/src/app/app.module.ts
+++ trip-forum/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
@@ -9,7 +11,7 @@ import { DestinationsListComponent } from './destinations-list/destinations-list
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, DestinationsListComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
