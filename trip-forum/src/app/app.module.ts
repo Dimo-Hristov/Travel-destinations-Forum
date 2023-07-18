4658a4e6498c7ac5ed2a69dcaef24be43ef90560
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-18 10:11:46 +0300

    generate destinations list component

--- trip-forum/src/app/app.module.ts
+++ trip-forum/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from './core/core.module';
+import { DestinationsListComponent } from './destinations-list/destinations-list.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, DestinationsListComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
