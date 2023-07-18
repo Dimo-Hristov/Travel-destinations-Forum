25a316e7b17d37a1c4c4d22d11a836ba7f97e693
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-18 11:21:52 +0300

    implement login method

--- trip-forum/src/app/shared/auth.service.ts
+++ trip-forum/src/app/shared/auth.service.ts
@@ -6,5 +6,17 @@ import { Router } from '@angular/router';
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private fireAuth: AngularFireAuth, private router: Router) {}
+  constructor(private fireAuth: AngularFireAuth, private router: Router) {
+
+    // login method
+    login(email: String , password: String){
+      this.fireAuth.signInWithEmailAndPassword(email, password).then(() => {
+        localStorage.setItem('token', 'true');
+        this.router.navigate(['/home'])
+      }, err =>{
+        alert('Something went wrong')
+        this.router.navigate(['/login'])
+      })
+    }
+  }
 }
