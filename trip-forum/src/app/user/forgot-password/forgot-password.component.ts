f75e7ef5ff53ece54f25c466081d657ab5c1d412
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-19 10:41:07 +0300

    create forgot password component, add form and implement logic

--- trip-forum/src/app/user/forgot-password/forgot-password.component.ts
+++ trip-forum/src/app/user/forgot-password/forgot-password.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.css']
+  styleUrls: ['./forgot-password.component.css'],
 })
 export class ForgotPasswordComponent {
+  constructor(private auth: AuthService) {}
 
+  forgotPasswordHandler(email: NgForm) {}
 }
