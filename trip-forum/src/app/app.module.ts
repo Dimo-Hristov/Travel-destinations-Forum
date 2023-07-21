08b4065a777f9efb63d9b2f84a8b295048e7b45c
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-21 09:09:10 +0300

    generate destinations-list and main components
    Render them in app component

--- trip-forum/src/app/app.module.ts
+++ trip-forum/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { MainComponent } from './main/main.component';
+import { DestinationsListComponent } from './destinations-list/destinations-list.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainComponent, DestinationsListComponent],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
