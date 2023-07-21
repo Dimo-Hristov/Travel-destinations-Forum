d589464dd43341369d877ada6a45751c174f3b09
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-21 09:20:02 +0300

    generate enviroments and add the appUrl

--- trip-forum/src/app/app.module.ts
+++ trip-forum/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainComponent } from './main/main.component';
 import { DestinationsListComponent } from './destinations-list/destinations-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, MainComponent, DestinationsListComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
