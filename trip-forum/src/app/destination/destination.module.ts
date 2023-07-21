5dd671c4f950e0341c830b4f80cb9e3a13989f3d
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-21 14:21:59 +0300

    generate add-destination component in destinations module

--- trip-forum/src/app/destination/destination.module.ts
+++ trip-forum/src/app/destination/destination.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddDestinationComponent } from './add-destination/add-destination.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddDestinationComponent
+  ],
   imports: [
     CommonModule
   ]
