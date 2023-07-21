02b56ad5c0bf10a6a550c5397b5fd863918b1e35
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-21 08:39:45 +0300

    remove the old project and start new with sortuni practice server

--- trip-forum/src/app/app-routing.module.ts
+++ trip-forum/src/app/app-routing.module.ts
@@ -1,24 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './user/login/login.component';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './user/register/register.component';
-import { ForgotPasswordComponent } from './user/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './user/verify-email/verify-email.component';
-import { DestinationsListComponent } from './destinations-list/destinations-list.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'verify-email', component: VerifyEmailComponent },
-  { path: 'destinations-list', component: DestinationsListComponent },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
