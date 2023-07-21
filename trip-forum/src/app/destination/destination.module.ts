ce112093ce3d2a5676e860c8aceb58b2788a93b4
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-21 14:27:03 +0300

    add current-destination component in destination module

--- trip-forum/src/app/destination/destination.module.ts
+++ trip-forum/src/app/destination/destination.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddDestinationComponent } from './add-destination/add-destination.component';
+import { CurrentDestinationComponent } from './current-destination/current-destination.component';
 
 
 
 @NgModule({
   declarations: [
-    AddDestinationComponent
+    AddDestinationComponent,
+    CurrentDestinationComponent
   ],
   imports: [
     CommonModule
