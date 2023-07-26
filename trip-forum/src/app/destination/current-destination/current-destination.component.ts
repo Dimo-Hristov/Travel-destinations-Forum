be190ef764b9fa279ccad6bc3d7b37c1af5af7ee
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-26 10:09:30 +0300

    fetch current destination and console.log it done

--- trip-forum/src/app/destination/current-destination/current-destination.component.ts
+++ trip-forum/src/app/destination/current-destination/current-destination.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { destination } from 'src/app/types/destination';
 
 @Component({
   selector: 'app-current-destination',
   templateUrl: './current-destination.component.html',
-  styleUrls: ['./current-destination.component.css']
+  styleUrls: ['./current-destination.component.css'],
 })
-export class CurrentDestinationComponent {
+export class CurrentDestinationComponent implements OnInit {
+  destination: destination | undefined;
 
+  constructor(
+    private apiService: ApiService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.fetchDestination();
+  }
+
+  fetchDestination(): void {
+    const id = this.activatedRoute.snapshot.params['destinationId'];
+    this.apiService.getDestination(id).subscribe((destination) => {
+      this.destination = destination;
+      console.log(destination);
+    });
+  }
 }
