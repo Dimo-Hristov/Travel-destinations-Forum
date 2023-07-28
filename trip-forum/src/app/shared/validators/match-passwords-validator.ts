668014ee6a43397cd0625dcb6698506c8c79a0bc
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-07-28 10:56:51 +0300

    add password and rePass validation
    at equired, at least 5 sumbols and password match func call

--- trip-forum/src/app/shared/validators/match-passwords-validator.ts
+++ trip-forum/src/app/shared/validators/match-passwords-validator.ts
@@ -1,10 +1,10 @@
 import { FormGroup, ValidatorFn } from '@angular/forms';
 
-export function passwordsValidator(pass: any, rePass: any): ValidatorFn {
+export function passwordsValidator(pass: string, rePass: string): ValidatorFn {
   return (control) => {
     const group = control as FormGroup;
     const pass1 = group.get(pass);
     const pass2 = group.get(rePass);
-    return pass.value === rePass.value ? null : { passwordsValidator: true };
+    return pass1?.value === pass2?.value ? null : { passwordsValidator: true };
   };
 }
