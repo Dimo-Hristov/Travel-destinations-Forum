bc21d21b9890ccefca363e52927bf808466275b1
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-08-04 09:46:17 +0300

    add destinations types in navbar

--- trip-forum/src/app/destination/scroll.service.ts
+++ trip-forum/src/app/destination/scroll.service.ts
@@ -1,14 +1,21 @@
-// scroll.service.ts
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScrollService {
   private lastScrollPosition: number = 0;
+  private previousUrl: string = '/';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private location: Location) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.previousUrl = event.url;
+      }
+    });
+  }
 
   setLastScrollPosition(position: number): void {
     this.lastScrollPosition = position;
@@ -20,24 +27,36 @@ export class ScrollService {
 
   goBackWithAnimation(): void {
     const lastScrollPosition = this.lastScrollPosition;
-    this.router.navigate(['/destinations']);
 
-    const duration = 500; // Time in milliseconds for the transition
-    const startTime = performance.now();
+    // Store the current URL before navigating back
+    const currentUrl = this.router.url;
 
-    const scrollAnimation = (currentTime: number) => {
-      const timeElapsed = currentTime - startTime;
-      const progress = Math.min(timeElapsed / duration, 1);
-      const ease = this.easeOutCubic(progress);
-      const newPosition = lastScrollPosition * ease;
-      window.scrollTo(0, newPosition);
+    // Navigate back to the previous URL using Location service
+    this.location.back();
 
-      if (timeElapsed < duration) {
+    // Wait for the back navigation to complete before starting the scroll animation
+    const checkNavigationCompleted = setInterval(() => {
+      if (this.router.url !== currentUrl) {
+        clearInterval(checkNavigationCompleted);
+
+        const duration = 500; // Time in milliseconds for the transition
+        const startTime = performance.now();
+
+        const scrollAnimation = (currentTime: number) => {
+          const timeElapsed = currentTime - startTime;
+          const progress = Math.min(timeElapsed / duration, 1);
+          const ease = this.easeOutCubic(progress);
+          const newPosition = lastScrollPosition * ease;
+          window.scrollTo(0, newPosition);
+
+          if (timeElapsed < duration) {
+            requestAnimationFrame(scrollAnimation);
+          }
+        };
+
         requestAnimationFrame(scrollAnimation);
       }
-    };
-
-    requestAnimationFrame(scrollAnimation);
+    }, 10);
   }
 
   // Easing function for smoother animation (Cubic Out)
