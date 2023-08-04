4b24210279cd1581dae680304f8e463c8d073821
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-08-04 11:54:39 +0300

    set global from styles.css google font roboto

--- trip-forum/src/app/core/core.module.ts
+++ trip-forum/src/app/core/core.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { SearchDirective } from './search.directive';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, SearchDirective],
   imports: [CommonModule, RouterModule],
   exports: [FooterComponent, HeaderComponent],
 })
