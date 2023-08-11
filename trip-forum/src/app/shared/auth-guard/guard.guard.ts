d38a69177a02b0fe0048cc26cdf54f06adf420a6
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-08-11 12:20:20 +0300

    add auth guard

---
+++ trip-forum/src/app/shared/auth-guard/guard.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from 'src/app/user/user.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private userService: UserService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    if (this.userService.isLogged) {
+      return true;
+    } else {
+      // If user is not logged in, redirect to login page or any other route
+      return this.router.createUrlTree(['/login']);
+    }
+  }
+}
