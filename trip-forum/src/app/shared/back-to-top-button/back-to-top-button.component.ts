7efad93c3f13649bb52efd6b1c12be09c819d882
Dimo-Hristov <114024778+Dimo-Hristov@users.noreply.github.com>
2023-08-11 11:56:08 +0300

    add html, css, and logic into scroll-to-top button component

--- trip-forum/src/app/shared/back-to-top-button/back-to-top-button.component.ts
+++ trip-forum/src/app/shared/back-to-top-button/back-to-top-button.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-back-to-top-button',
   templateUrl: './back-to-top-button.component.html',
-  styleUrls: ['./back-to-top-button.component.css']
+  styleUrls: ['./back-to-top-button.component.css'],
 })
 export class BackToTopButtonComponent {
-
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth', // Add smooth scrolling effect
+    });
+  }
 }
